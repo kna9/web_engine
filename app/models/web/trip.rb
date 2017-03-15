42e867d78dce7e59340eb746238d75ec0bc756a2
Karim Naghmouchi <karim@ecov.fr>
2017-03-15 19:34:14 +0100

    maj bug trip

--- app/models/web/trip.rb
+++ app/models/web/trip.rb
@@ -5,20 +5,31 @@ module Web
     # FIXME : un peu de copier coller pour ces methodes a supprimer...
     # FIXME : extraire la config dans un fichier ou aller chercher la config du SI
     def estimated_waiting_time(dow, hour)
-      waiting_time = times[dow.to_s][hour]
+      processed_times = JSON.parse(times) if times
 
-      if waiting_time.nil? || waiting_time == -1 || waiting_time > 3600
-        3600
-      else
-        waiting_time
+      if processed_times
+        waiting_time = processed_times[dow][hour] if processed_times[dow]
+        if waiting_time.nil? || waiting_time == -1 || waiting_time > 3600
+          3600
+        else
+          waiting_time
+        end
       end
     end
+
 
     # FIXME : un peu de copier coller pour ces methodes a supprimer...
     # FIXME : extraire la config dans un fichier ou aller chercher la config du SI
     def estimated_flow(dow, hour)
-      flow[dow.to_s][hour]
+      # flow[dow.to_s][hour]
+      return unless flow
+      res_flow = JSON.parse(flow)
+
+      ret = res_flow[dow.to_s][hour].to_i if res_flow && res_flow[dow.to_s]
+
+      return ret
     end
+
 
     # FIXME : un peu de copier coller pour ces methodes a supprimer...
     # FIXME : extraire la config dans un fichier ou aller chercher la config du SI
