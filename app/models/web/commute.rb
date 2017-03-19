b6bc26446318663e8776fe687ebea96d05f3eb84
Karim Naghmouchi <karim@ecov.fr>
2017-03-19 20:13:43 +0100

    maj commute

--- app/models/web/commute.rb
+++ app/models/web/commute.rb
@@ -1,17 +1,15 @@
 module Web
-  # FIME : pour ne pas fixer sur a carte des points déclarés qui ne sont pas dans les distances : récupérer les coordonnées et raccrocher au point le plus proche
   # Web::Destination.by_distance(:origin => [49.147956, 1.994782]).first
 
-
-
   class Commute < ActiveRecord::Base
+    SPEED_AVERAGE = 50
+    KM_DETOUR     = 5
+
     include Concerns::HasSiSynchronization
 
     def user
       Web::User.where(id: user_id).first
     end
-
-    # necessary--------------------
 
     def detours
       detours = []
@@ -55,111 +53,24 @@ module Web
     #   Web::Commute.first(d: self.id)
     # end
 
-    #-----------------------------
-
     alias_method :from, :start
     alias_method :from_city, :start
     alias_method :to, :end
     alias_method :to_city, :end
 
     QUARTERS =  [
-        ['0:00', '0:15'],
-        ['0:15', '0:30'],
-        ['0:30', '0:45'],
-        ['0:45', '1:00'],
-        ['1:00', '1:15'],
-        ['1:15', '1:30'],
-        ['1:30', '1:45'],
-        ['1:45', '2:00'],
-        ['2:00', '2:15'],
-        ['2:15', '2:30'],
-        ['2:30', '2:45'],
-        ['2:45', '3:00'],
-        ['3:00', '3:15'],
-        ['3:15', '3:30'],
-        ['3:30', '3:45'],
-        ['3:45', '4:00'],
-        ['4:00', '4:15'],
-        ['4:15', '4:30'],
-        ['4:30', '4:45'],
-        ['4:45', '5:00'],
-        ['5:00', '5:15'],
-        ['5:15', '5:30'],
-        ['5:30', '5:45'],
-        ['5:45', '6:00'],
-        ['6:00', '6:15'],
-        ['6:15', '6:30'],
-        ['6:30', '6:45'],
-        ['6:45', '7:00'],
-        ['7:00', '7:15'],
-        ['7:15', '7:30'],
-        ['7:30', '7:45'],
-        ['7:45', '8:00'],
-        ['8:00', '8:15'],
-        ['8:15', '8:30'],
-        ['8:30', '8:45'],
-        ['8:45', '9:00'],
-        ['9:00', '9:15'],
-        ['9:15', '9:30'],
-        ['9:30', '9:45'],
-        ['9:45', '10:00'],
-        ['10:00', '11:15'],
-        ['10:15', '10:30'],
-        ['10:30', '10:45'],
-        ['10:45', '11:00'],
-        ['11:00', '11:15'],
-        ['11:15', '11:30'],
-        ['11:30', '11:45'],
-        ['11:45', '12:00'],
-        ['12:00', '12:15'],
-        ['12:15', '12:30'],
-        ['12:30', '12:45'],
-        ['12:45', '13:00'],
-        ['13:00', '13:15'],
-        ['13:15', '13:30'],
-        ['13:30', '13:45'],
-        ['13:45', '14:00'],
-        ['14:00', '14:15'],
-        ['14:15', '14:30'],
-        ['14:30', '14:45'],
-        ['14:45', '15:00'],
-        ['15:00', '15:15'],
-        ['15:15', '15:30'],
-        ['15:30', '15:45'],
-        ['15:45', '16:00'],
-        ['16:00', '16:15'],
-        ['16:15', '16:30'],
-        ['16:30', '16:45'],
-        ['16:45', '17:00'],
-        ['17:00', '17:15'],
-        ['17:15', '17:30'],
-        ['17:30', '17:45'],
-        ['17:45', '18:00'],
-        ['18:00', '18:15'],
-        ['18:15', '18:30'],
-        ['18:30', '18:45'],
-        ['18:45', '19:00'],
-        ['19:00', '19:15'],
-        ['19:15', '19:30'],
-        ['19:30', '19:45'],
-        ['19:45', '20:00'],
-        ['20:00', '20:15'],
-        ['20:15', '20:30'],
-        ['20:30', '20:45'],
-        ['20:45', '21:00'],
-        ['21:00', '21:15'],
-        ['21:15', '21:30'],
-        ['21:30', '21:45'],
-        ['21:45', '22:00'],
-        ['22:00', '22:15'],
-        ['22:15', '22:30'],
-        ['22:30', '22:45'],
-        ['22:45', '23:00'],
-        ['23:00', '23:15'],
-        ['23:15', '23:30'],
-        ['23:30', '23:45'],
-        ['23:45', '0:00']
-      ]
+      ['0:00', '0:15'], ['0:15', '0:30'], ['0:30', '0:45'], ['0:45', '1:00'], ['1:00', '1:15'], ['1:15', '1:30'], ['1:30', '1:45'], ['1:45', '2:00'],
+      ['2:00', '2:15'], ['2:15', '2:30'], ['2:30', '2:45'], ['2:45', '3:00'], ['3:00', '3:15'], ['3:15', '3:30'], ['3:30', '3:45'], ['3:45', '4:00'],
+      ['4:00', '4:15'], ['4:15', '4:30'], ['4:30', '4:45'], ['4:45', '5:00'], ['5:00', '5:15'], ['5:15', '5:30'], ['5:30', '5:45'], ['5:45', '6:00'],
+      ['6:00', '6:15'], ['6:15', '6:30'], ['6:30', '6:45'], ['6:45', '7:00'], ['7:00', '7:15'], ['7:15', '7:30'], ['7:30', '7:45'], ['7:45', '8:00'],
+      ['8:00', '8:15'], ['8:15', '8:30'], ['8:30', '8:45'], ['8:45', '9:00'], ['9:00', '9:15'], ['9:15', '9:30'], ['9:30', '9:45'], ['9:45', '10:00'],
+      ['10:00', '11:15'], ['10:15', '10:30'], ['10:30', '10:45'], ['10:45', '11:00'], ['11:00', '11:15'], ['11:15', '11:30'], ['11:30', '11:45'], ['11:45', '12:00'],
+      ['12:00', '12:15'], ['12:15', '12:30'], ['12:30', '12:45'], ['12:45', '13:00'], ['13:00', '13:15'], ['13:15', '13:30'], ['13:30', '13:45'], ['13:45', '14:00'],
+      ['14:00', '14:15'], ['14:15', '14:30'], ['14:30', '14:45'], ['14:45', '15:00'], ['15:00', '15:15'], ['15:15', '15:30'], ['15:30', '15:45'], ['15:45', '16:00'],      ['16:00', '16:15'], ['16:15', '16:30'], ['16:30', '16:45'], ['16:45', '17:00'], ['17:00', '17:15'], ['17:15', '17:30'], ['17:30', '17:45'], ['17:45', '18:00'],
+      ['18:00', '18:15'], ['18:15', '18:30'], ['18:30', '18:45'], ['18:45', '19:00'], ['19:00', '19:15'], ['19:15', '19:30'], ['19:30', '19:45'], ['19:45', '20:00'],
+      ['20:00', '20:15'], ['20:15', '20:30'], ['20:30', '20:45'], ['20:45', '21:00'], ['21:00', '21:15'], ['21:15', '21:30'], ['21:30', '21:45'], ['21:45', '22:00'],
+      ['22:00', '22:15'], ['22:15', '22:30'], ['22:30', '22:45'], ['22:45', '23:00'], ['23:00', '23:15'], ['23:15', '23:30'], ['23:30', '23:45'], ['23:45', '0:00']
+    ]
 
     # def time
     #   super.utc.strftime("%H:%M")
@@ -169,12 +80,24 @@ module Web
       ((time2 - time1) / 60).to_i
     end
 
+    def passage_time_engagement(location)
+      ((passage_time2(location) - passage_time1(location)) / 60).to_i
+    end
+
     def time1
-      time-time_delta.minutes
+      time - time_delta.minutes
     end
 
     def time2
       time + time_delta.minutes
+    end
+
+    def passage_time1(location) 
+      passage_time_to_station(location) - time_delta.minutes
+    end
+
+    def passage_time2(location)
+      passage_time_to_station(location) + time_delta.minutes
     end
 
     def ec
@@ -236,6 +159,26 @@ module Web
       end 
     end
 
+    def display_passage_time(location)
+      if passage_time_to_station(location)
+        formated_time(passage_time_to_station(location)) 
+      end
+    end
+
+    def display_passage_time_min(location)
+      if passage_time_to_station(location)
+        passage_time_min = passage_time1(location)
+        formated_time(passage_time_min)
+      end   
+    end
+
+    def display_passage_time_max(location)
+      if passage_time_to_station(location)
+        passage_time_max = passage_time2(location)
+        formated_time(passage_time_max)
+      end 
+    end
+
     def display_departure_time
       formated_time(time)   
     end
@@ -255,201 +198,190 @@ module Web
     end
 
     def self.get_commutes(args = {})
-      choosen_web_station = args[:location]
-    
-      return self.all unless choosen_web_station
-    
+      location       = args[:location]
+      destination    = args[:destination]
       day_of_week    = args[:dow]
       departure_time = args[:time]
-    
-      km_detour_approximation = 1 # FIXME: depends on detour declared (en temps donc simuler vitesse)
-    
-      near_choosen_location_stations = Web::Location.within(km_detour_approximation, origin: choosen_web_station)
-      near_choosen_location_steps    = Web::Destination.within(km_detour_approximation, origin: choosen_web_station)
-    
-      if args[:destination]
-        choosen_web_destination           = args[:destination]
-        near_choosen_destination_stations = Web::Location.within(km_detour_approximation, origin: choosen_web_destination)
-        near_choosen_destination_steps    = Web::Destination.within(km_detour_approximation, origin: choosen_web_destination)
-      end
-    
-      near_stations     = (near_choosen_location_stations.to_a + near_choosen_destination_stations.to_a).uniq
-      near_steps        = (near_choosen_location_steps.to_a + near_choosen_destination_steps).uniq
-      near_stations_ids = near_stations.map(&:id)
-      near_steps_ids    = near_steps.map(&:id)
-      acm               = []
 
+      departure_time = departure_time.to_time if departure_time
 
-      # FIXME : 
+      return [] unless location
+      return [] unless destination
 
-      # near_stations = []
-      # near_stations_ids = []
+      results = []
 
-          
+      pre_results = []
       Web::Commute.all.each do |commute|
+        pre_results << commute if commute.compatible_dow?(day_of_week)
+      end
+
+      pre_results.each do |commute|
         commute_ok = false
-    
-        if near_steps_ids.include?(commute.start_id) || near_steps_ids.include?(commute.end_id) || (commute.detours.map(&:id) & near_steps_ids).any?
-          commute_ok = true 
-        end
-    
-        if (commute.stations.map(&:id) & near_stations_ids).any?
-          commute_ok = true 
-        end
-    
-        # --exclusions conditions with day_of_week || departure_time
-    
-        if day_of_week && day_of_week.any? 
-          # unless (day_of_week & commute.dow).any? # [0,1,2,3]
-          unless (day_of_week & (commute.dow ? commute.dow : [])).any? # [0,1,2,3]
-            commute_ok = false
-          end 
-        end
-    
-        if departure_time
-          si_time = commute.time # "08:30".to_time
-          if si_time
-            si_time = si_time.to_time
-          end
-    
-          time_delta = commute.time_delta
-          time_delta = 0 unless time_delta
-          time_delta = time_delta.to_i
-          min_time = si_time-time_delta.seconds
-          max_time = si_time + time_delta.seconds 
+        commute_ok = true if commute.passage_time_to_station(location) && commute.arrival_compatible(destination)
+        
+        passage_time1 = commute.time - commute.time_delta.minutes
+        passage_time2 = commute.time + commute.time_delta.minutes
 
-          # FIXME : ICI ON PEUT REGLER SECONDES DELTA EN MINUTES SI PAS ASSEZ DE RESULT
-          # min_time = si_time-time_delta.minutes
-          # max_time = si_time + time_delta.minutes
-    
-          if departure_time.utc.strftime("%H%M%S%N") < min_time.utc.strftime("%H%M%S%N") || departure_time.utc.strftime("%H%M%S%N") > max_time.utc.strftime("%H%M%S%N")
-            commute_ok = false
-          end
+        if commute.from && departure_time && passage_time1 && passage_time2
+          dt = departure_time.to_s.split(" ")[1].to_time
+          t1 = passage_time1.to_s.split(" ")[1].to_time
+          t2 = passage_time2.to_s.split(" ")[1].to_time
+          commute_ok = false if dt <  t1 || dt > t2
         end
-    
-        if commute_ok == true
-          acm << commute
-        end
+
+        results << commute if commute_ok
       end
-    
-      return acm
+
+      return results
     end
 
-    def self.create_all_commute_steps_report
+    def arrival_compatible(arrival, detour_km = KM_DETOUR)
+      theorical_waypoints.reverse_each do |commute_waypoint|
+        distance_from_waypoint_to_arrival_wich = commute_waypoint.distance_from(arrival).to_f 
+        return true if distance_from_waypoint_to_arrival_wich <= detour_km
+      end
+
+      return false 
+    end
+
+    def convert_coordinates_array_to_waypoints_array(coordinates_array)
+      waypoints_array = []
+        coordinates_array.each do |coordinates_string|
+        waypoints_array << Web::Destination.new(lat: coordinates_string.split(',').first, long: coordinates_string.split(',').last)
+      end
+
+      return waypoints_array
+    end
+
+    def passage_time_to_station(location, detour_km = KM_DETOUR, average_speed= SPEED_AVERAGE, debug = false)
+      key_cash = "#{location.id.to_s}-#{detour_km}-#{average_speed}"
+      @estimated_time_of_passage = {} unless @estimated_time_of_passage
+
+      return @estimated_time_of_passage[key_cash] if  @estimated_time_of_passage && @estimated_time_of_passage[key_cash]
       
-      # Web::CommutesReport::WebCommutesRepor
-      data_report = {}
+      estimated_time =  time_from_departure_to_station(location, detour_km, average_speed)
+      
+      @estimated_time_of_passage[key_cash] = nil unless estimated_time
+      @estimated_time_of_passage[key_cash] = nil unless time
+      
+      @estimated_time_of_passage[key_cash] = time + estimated_time.hours if estimated_time && time
+      
+      return @estimated_time_of_passage[key_cash]
+    end
 
-      secure_key = Web::CommutesReport.initialize_report('Tous les trajets déclarés')
+    def time_from_departure_to_station(location, detour_km = KM_DETOUR, average_speed= SPEED_AVERAGE)
+      estimated_distance = distance_from_departure_to_station(location, detour_km)
 
-      self.all.each do |web_commute|
-        web_commute.steps_with_times.each do |step|
-          if step.any? && step.count == 2 && step.first.first && step.last.first
-            t1 = step.first.last
-            t2 = step.last.last
+      return nil unless estimated_distance
+      
+      estimated_time = estimated_distance / average_speed
 
-            report_key = "#{step.first.first.id}_#{step.last.first.id}_#{t1}_#{t2}"
+      return estimated_time
+    end
 
-            if data_report[report_key] 
-              data_report[report_key][0] = data_report[report_key][0].to_i + 1
-            else
-              data_report[report_key] = [1, [step.first.first, step.first.last], [step.last.first, step.last.last]]
-            end
-          end
+    def distance_from_departure_to_station(location, detour_km = KM_DETOUR)
+      array_points_distance = []
+
+      theorical_waypoints.each do |theorical_waypoint|
+        theorical_distance = theorical_waypoint.distance_from(location).to_f 
+
+        if (theorical_distance <= detour_km)
+          array_points_distance << [ theorical_distance, theorical_waypoint ]
         end
       end
 
-      data_report.each do |key, value|
-        if value && value.any? && value[0] && value[1] && value[1].any? && value[1][0] && value[1][1]&& value[2] && value[2].any? && value[2][0] && value[2][1]
-          wc = CommutesReport.create(
-            report_key: secure_key, 
-            quantity:   value[0],
-            from_id:    value[1][0].id, 
-            from_name:  value[1][0].name, 
-            from_lat:   value[1][0].lat, 
-            from_lng:   value[1][0].lng, 
-            from_time:  value[1][1], 
-            to_id:      value[2][0].id, 
-            to_name:    value[2][0].name, 
-            to_lat:     value[2][0].lat, 
-            to_lng:     value[2][0].lng,
-            to_time:    value[2][1]
-          )
-        end
+      array_points_distance   = array_points_distance.sort { |a, b| a.first <=> b.first }
+      near_point_and_distance = array_points_distance.first
+
+      return nil unless near_point_and_distance
+
+      near_point_distance_from_station = near_point_and_distance.first
+      near_point_from_station          = near_point_and_distance.last
+
+      distance_from_departure_to_waypoint = near_point_from_station.distance_from(from)
+
+      distance_from_departure_to_station  = distance_from_departure_to_waypoint + near_point_distance_from_station
+
+      return distance_from_departure_to_station
+    end
+
+    def theorical_waypoints
+      return @theorical_waypoints if @theorical_waypoints
+
+      @theorical_waypoints   = []
+      @theorical_waypoints << from
+      
+      theorical_coordinates = GeoProcessService.new.get_waypoints(from.lat, from.long, to.lat, to.long)
+         
+      theorical_coordinates.each do |coordinates_string|
+        @theorical_waypoints << Web::Destination.new(lat: coordinates_string.split(',').first, long: coordinates_string.split(',').last)
       end
 
-      secure_key
+      @theorical_waypoints << to
+
+      return @theorical_waypoints
     end
-
-    def steps_with_times(speed_average = 90)
-      steps_with_times = []
-
-      old_time = self.time ? self.time : '00:00'.to_time
-      old_city = self.from_city
-      detours  = self.detours
-
-      begin
-        part_cities  = []
-
-        part_cities << [ old_city, short_time(old_time) ]
-
-        detours.each do |detour_city|
-          new_time = short_time(old_time).to_time + detour_city.time_from(old_city, speed_average)
-
-          part_cities << [ detour_city, short_time(new_time)]
-
-          steps_with_times << part_cities
-
-          old_time = new_time
-          old_city = detour_city
-
-          part_cities = []
-          part_cities << [ detour_city, short_time(old_time)]
-        end
-
-        new_time = short_time(old_time).to_time + self.to_city.time_from(old_city, speed_average)
-        part_cities << [ self.to_city, short_time(new_time) ]
-
-        steps_with_times << part_cities
-      rescue
-        steps_with_times = if self.from_city && self.to_city
-          [
-            [self.from_city, short_time(old_time)], 
-            [self.to_city, short_time(short_time(old_time).to_time + self.to_city.time_from(self.from_city, speed_average))] 
-          ]
-        else 
-          []
-        end
-      end
-
-
-      steps_with_times
-    end
-
-    def suggested_web_stations(km_zone = 3)
-      compatible_web_stations(km_zone)-self.stations
-    end
-
-    def compatible_web_stations(km_zone = 3)
-      compatible_web_stations = []
-
-      Location.all.each do |web_station|
-        self.detours.each do |step|
-          if step.distance_from(web_station).to_f <= km_zone.to_f
-            compatible_web_stations << web_station unless compatible_web_stations.include?(web_station)
-          end
-        end
-      end
-
-      compatible_web_stations
-    end
-
-    private
 
     # FIXME : short_time : sortir dans un helper
-
     def short_time(real_time)
       (real_time && real_time.to_time) ? real_time.strftime("%H:%M") : "00:00"
+    end
+
+    def compatible_departure_time?(departure_time)
+      compatible_departure_time = true
+      min_time, max_time        = get_min_and_max_time
+
+      # "08:30".to_time.utc.strftime("%H%M%S%N")
+
+      return false unless departure_time
+
+      # FIXME : ICI ON PEUT REGLER SECONDES DELTA EN MINUTES SI PAS ASSEZ DE RESULT / min_time = si_time-time_delta.minutes / max_time = si_time + time_delta.minutes
+  
+      if process_converted_time < min_time || process_converted_time > max_time
+        compatible_departure_time = false
+      end
+
+      return compatible_departure_time
+    end
+
+    def get_min_and_max_time
+      si_time = time # "08:30".to_time
+      if si_time
+        si_time = si_time.to_time
+      end
+  
+      time_delta = time_delta
+      time_delta = 0 unless time_delta
+      time_delta = time_delta.to_i
+
+      min_time = si_time - time_delta.seconds
+      max_time = si_time + time_delta.seconds 
+
+      [convert_time(min_time), convert_time(max_time)]
+    end
+
+    def process_converted_time
+      convert_time(time)
+    end
+
+    def convert_time(t)
+      t.utc.strftime("%H%M%S%N")
+    end
+
+    def converted_time
+      time.utc.strftime("%H:%M")
+    end
+
+    def compatible_dow?(day_of_week)
+      compatible_dow = true
+
+      if day_of_week && day_of_week.any? 
+        unless (day_of_week & (dow ? dow : [])).any? 
+          compatible_dow = false
+        end 
+      end
+
+      return compatible_dow
     end
   end
 end
