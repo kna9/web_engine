e2e65131e1a77c35ee04eae1edea2655061e3b35
Karim Naghmouchi <karim@ecov.fr>
2017-04-08 08:49:38 +0200

    remove address process

--- app/models/web/location.rb
+++ app/models/web/location.rb
@@ -2,16 +2,5 @@ module Web
   class Location < ActiveRecord::Base
     include Concerns::HasGeocodingProperties
     include Concerns::HasSiSynchronization
-
-    def formatted_address
-      station_address = nil
-      station_geocode = JSON.load(open("http://maps.googleapis.com/maps/api/geocode/json?latlng=#{lat},#{long}&sensor=true_or_false"))
-
-      if station_geocode && station_geocode['results'] && station_geocode['results'].any? && station_geocode['results'].first && station_geocode['results'].first['formatted_address']
-        station_address = station_geocode['results'].first['formatted_address']
-      end
-
-      return station_address
-    end
   end
 end
