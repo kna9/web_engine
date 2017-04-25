a345008f633d2a8aaff1e7d342c352d1f849d0d8
Karim Naghmouchi <karim@ecov.fr>
2017-04-25 16:30:33 +0200

    prepare trip aggregation

--- app/models/web/trip.rb
+++ app/models/web/trip.rb
@@ -1,6 +1,20 @@
 module Web
   class Trip < ActiveRecord::Base
     include Concerns::HasSiSynchronization
+
+    #    def self.max_flow_for_location(location, dow = 2)
+    #      all_trips = Web::Trip.where(location_id: location.id)
+    #      puts all_trips.count
+    #      flws     = []
+    #
+    #      all_trips.each do |trip|
+    #        JSON.parse(trip.flow)[dow.to_s].each do |f|
+    #          flws << f.to_f 
+    #        end
+    #      end
+    #
+    #      return flws.uniq.map(&:to_f).max
+    #    end
     
     # FIXME : un peu de copier coller pour ces methodes a supprimer...
     # FIXME : extraire la config dans un fichier ou aller chercher la config du SI
@@ -44,3 +58,12 @@ module Web
     end
   end
 end
+
+figures = []
+Web::Location.all.each do |l|
+  figures << [l.name, Web::Trip.flow_for_location(Web::Location.first, 3)]
+end;nil
+
+figures.each do |f|
+  puts "#{f.first}  : #{f.last}"
+end;nil
