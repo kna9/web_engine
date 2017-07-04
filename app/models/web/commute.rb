cfd1aadf5ed771bef35ec66b8de28dc43ab25aa9
Karim Naghmouchi <karim@ecov.fr>
2017-07-04 17:00:50 +0200

    retours PR

--- app/models/web/commute.rb
+++ app/models/web/commute.rb
@@ -2,15 +2,12 @@ module Web
   # Web::Destination.by_distance(:origin => [49.147956, 1.994782]).first
 
   class Commute < ActiveRecord::Base
-    SPEED_AVERAGE = 50
-    KM_DETOUR     = 5
+    SPEED_AVERAGE      = 50
+    KM_DETOUR          = 5
+    MILES_PER_KM       = 1.6
+    DETOUR_DELTA_LIMIT = 20 # 10
 
     include Concerns::HasSiSynchronization
-
-    #after_save :save_all_commute_locations
-    #after_create :save_all_commute_locations
-
-    # FIXME : test and add after_save (save_all_commute_locations)
 
     def save_all_commute_locations
       Web::Location.all.each do |location|
@@ -35,7 +32,16 @@ module Web
 
     def save_detours(commute_id)
       if commute_id
-        ActiveRecord::Base.connection.execute("delete from db_commute_destinations where commute_user_id=#{user_id} and commute_id=#{commute_id}")
+        connection = ActiveRecord::Base.connection.raw_connection
+        sql = "delete from db_commute_destinations where commute_user_id=$1 and commute_id=$2"
+        
+        begin
+          connection.prepare('del_commute_destination', sql)
+        rescue PG::DuplicatePstatement => e
+        end
+        
+        st = connection.exec_prepared('del_commute_destination', [ user_id, commute_id ])
+
         detours.each do |detour_destination|
           location_to_save = DbCommuteDestination.new(commute_user_id: user_id, commute_id: commute_id, detour_id: detour_destination.id)
           begin
@@ -49,7 +55,17 @@ module Web
 
     def save_stations(commute_id)
       if commute_id
-        ActiveRecord::Base.connection.execute("delete from db_commute_locations where commute_user_id=#{user_id} and commute_id=#{commute_id}")
+
+        connection = ActiveRecord::Base.connection.raw_connection
+        sql = "delete from db_commute_locations where commute_user_id=$1 and commute_id=$2"
+        
+        begin
+          connection.prepare('del_commute_location', sql)
+        rescue PG::DuplicatePstatement => e
+        end
+
+        st = connection.exec_prepared('del_commute_location', [ user_id, commute_id ])
+
         stations.each do |station_location|
           station_to_save = DbCommuteLocation.new(commute_user_id: user_id, commute_id: commute_id, station_id: station_location.id)
           begin
@@ -102,36 +118,21 @@ module Web
     alias_method :to, :end
     alias_method :to_city, :end
 
-    QUARTERS =  [
-      ['0:00', '0:15'], ['0:15', '0:30'], ['0:30', '0:45'], ['0:45', '1:00'], ['1:00', '1:15'], ['1:15', '1:30'], ['1:30', '1:45'], ['1:45', '2:00'],
-      ['2:00', '2:15'], ['2:15', '2:30'], ['2:30', '2:45'], ['2:45', '3:00'], ['3:00', '3:15'], ['3:15', '3:30'], ['3:30', '3:45'], ['3:45', '4:00'],
-      ['4:00', '4:15'], ['4:15', '4:30'], ['4:30', '4:45'], ['4:45', '5:00'], ['5:00', '5:15'], ['5:15', '5:30'], ['5:30', '5:45'], ['5:45', '6:00'],
-      ['6:00', '6:15'], ['6:15', '6:30'], ['6:30', '6:45'], ['6:45', '7:00'], ['7:00', '7:15'], ['7:15', '7:30'], ['7:30', '7:45'], ['7:45', '8:00'],
-      ['8:00', '8:15'], ['8:15', '8:30'], ['8:30', '8:45'], ['8:45', '9:00'], ['9:00', '9:15'], ['9:15', '9:30'], ['9:30', '9:45'], ['9:45', '10:00'],
-      ['10:00', '11:15'], ['10:15', '10:30'], ['10:30', '10:45'], ['10:45', '11:00'], ['11:00', '11:15'], ['11:15', '11:30'], ['11:30', '11:45'], ['11:45', '12:00'],
-      ['12:00', '12:15'], ['12:15', '12:30'], ['12:30', '12:45'], ['12:45', '13:00'], ['13:00', '13:15'], ['13:15', '13:30'], ['13:30', '13:45'], ['13:45', '14:00'],
-      ['14:00', '14:15'], ['14:15', '14:30'], ['14:30', '14:45'], ['14:45', '15:00'], ['15:00', '15:15'], ['15:15', '15:30'], ['15:30', '15:45'], ['15:45', '16:00'],      
-      ['16:00', '16:15'], ['16:15', '16:30'], ['16:30', '16:45'], ['16:45', '17:00'], ['17:00', '17:15'], ['17:15', '17:30'], ['17:30', '17:45'], ['17:45', '18:00'],
-      ['18:00', '18:15'], ['18:15', '18:30'], ['18:30', '18:45'], ['18:45', '19:00'], ['19:00', '19:15'], ['19:15', '19:30'], ['19:30', '19:45'], ['19:45', '20:00'],
-      ['20:00', '20:15'], ['20:15', '20:30'], ['20:30', '20:45'], ['20:45', '21:00'], ['21:00', '21:15'], ['21:15', '21:30'], ['21:30', '21:45'], ['21:45', '22:00'],
-      ['22:00', '22:15'], ['22:15', '22:30'], ['22:30', '22:45'], ['22:45', '23:00'], ['23:00', '23:15'], ['23:15', '23:30'], ['23:30', '23:45'], ['23:45', '0:00']
-    ]
-
     ################## REPORTS & EQUIVALENT CONDUCTEURS
 
     def time_engagement
-      ((time2 - time1) / 60).to_i
+      ((time2 - time1) / 60).to_i # FIXME : le /60 : c'est parcequ'on est en secondes...???
     end
 
     def passage_time_engagement(location)
       ((passage_time2(location) - passage_time1(location)) / 60).to_i
     end
 
-    def ec
+    def ec # FIXME : change ec >> driver_equivalant
       ec = {}
 
-      QUARTERS.each do |t|
-        ec[t] = process_ec(t)
+      quarters_times_intervals.each do |t|
+        ec[t] = process_driver_equivalant(t.first.to_time, t.last.to_time)
       end
 
       ec
@@ -147,47 +148,26 @@ module Web
       ec_sum
     end
 
-    def ec_moy
-      QUARTERS.count.to_i == 0 ? 0 : ec_sum / QUARTERS.count.to_i
-    end
-
     def converted_time
       time.utc.strftime("%H:%M")
     end
 
-    def process_ec(times)
-      retour = 0
-
-
-      if (times.first.to_time..times.last.to_time).include?(converted_time.to_time)
-        retour = param_a + param_b
-      end
-
-      return retour
+    def process_driver_equivalant(time_min, time_max)
+      (time_min..time_max).include?(converted_time.to_time) ? driver_equivalant_value : 0
     end
 
-    def param_a
-      if time_engagement > 0 && time_engagement <= 15
-        1
+    def driver_equivalant_value
+      probability, weighting = if time_engagement > 0 && time_engagement <= 15
+        [ 1, 1 ]
       elsif time_engagement > 15 && time_engagement <= 30
-        0.7
+        [ 0.7, 0.5 ]
       elsif time_engagement > 30 && time_engagement <= 60
-        0.5
+        [ 0.5, 0.25 ]
       else
-        0
+        [ 0, 0 ]
       end
-    end
 
-    def param_b
-      if time_engagement > 0 && time_engagement <= 15
-        1
-      elsif time_engagement > 15 && time_engagement <= 30
-        0.5
-      elsif time_engagement > 30 && time_engagement <= 60
-        0.25
-      else
-        0
-      end 
+      probability * weighting
     end
 
     ################## TIME CONVERSION
@@ -255,17 +235,17 @@ module Web
     def self.search_commutes(args = {})
       whish_location       = args[:location]
       whish_destination    = args[:destination]
-      whish_day_of_week    = args[:dow]
+      whish_days_of_week    = args[:dow]
 
       whish_departure_time     = args[:time].to_time if args[:time]
       param_whish_departure_time_max = args[:time_max].to_time if args[:time_max]
 
       return [] unless whish_location
       return [] unless whish_destination
-      return [] unless whish_day_of_week
+      return [] unless whish_days_of_week
       return [] unless whish_departure_time
 
-      whish_departure_time    = extract_utc_time(whish_departure_time.utc)
+      whish_departure_time     = extract_utc_time(whish_departure_time.utc)
       whish_departure_time_min = whish_departure_time - 30.minutes
       whish_departure_time_max = whish_departure_time + 30.minutes
 
@@ -281,10 +261,13 @@ module Web
       begin
         whish_itinerary = GeoProcessService.new.osrm_itinerary(whish_location, whish_destination)
       rescue
+        # FIXME : Sentry send OSRM KO
         whish_itinerary = GeoProcessService.new.theorical_itinerary(whish_location, whish_destination)
       end
 
-      Web::Commute.dow_compatible(whish_day_of_week).each do |commute|
+      # raise Web::Commute.declared_for_dows(whish_days_of_week).map(&:id).include?(146).inspect
+
+      Web::Commute.declared_for_dows(whish_days_of_week).each do |commute|
         results << commute if commute.compatible_departure_and_arrival_with_whish_itinerary(whish_itinerary, whish_departure_time_min, whish_departure_time_max)
       end
 
@@ -293,11 +276,11 @@ module Web
 
     ##################
 
-    def self.dow_compatible(day_of_week)
+    def self.declared_for_dows(days_ids)
       compatible_dow_commutes = []
 
       Web::Commute.all.each do |commute|
-        compatible_dow_commutes << commute if commute.compatible_dow?(day_of_week)
+        compatible_dow_commutes << commute if commute.is_declared_for_dows?(days_ids)
       end
 
       return compatible_dow_commutes
@@ -321,6 +304,7 @@ module Web
 
     def compatible_departure_and_arrival_with_whish_itinerary(whish_itinerary, whish_departure_time_min, whish_departure_time_max)
       index_of_departure = index_of_passage(whish_itinerary.waypoints.first)
+      #raise index_of_departure.inspect
       index_of_arrival   = index_of_passage(whish_itinerary.waypoints.last)
 
       return false unless index_of_departure
@@ -338,6 +322,7 @@ module Web
       max_time = Web::Commute.extract_utc_time(waypoint_passage_time) + time_delta.minutes
       
       compatible_departure = (min_time >= whish_departure_time_min && min_time <= whish_departure_time_max) || (max_time >= whish_departure_time_min && max_time <= whish_departure_time_max)
+      # FIXME : tester et vérif : !(max_time < whish_departure_time_min || min_time > whish_departure_time_max) (manque des bornes)
 
       return compatible_departure
     end
@@ -345,28 +330,46 @@ module Web
     def index_of_passage(passage_waypoint)
       distances_and_indexes = []
 
+      dtcc = []
+
+      # if id == 146
+      #   raise passage_waypoint.inspect
+      # end
+
       itinerary.waypoints.each_with_index do |waypoint, index|
-        processed_distance = waypoint.distance_from(passage_waypoint).to_f * 1.6
+        # FIXME : modifier la conf pour avoir les distances en KM
+        processed_distance = waypoint.distance_from(passage_waypoint).to_f * 1.6 # * MILES_PER_KM
+
+        #raise processed_distance.inspect
+        #raise id.inspect
+
+        
+        # if id == 146
+        #   dtcc << processed_distance
+        # end
 
         if processed_distance <= detour_kilometers
+          #raise 'ok'
           distances_and_indexes << [processed_distance, index]
         end
       end
 
+      # raise dtcc.inspect if id == 146
+
       return distances_and_indexes.any? ? distances_and_indexes.sort.first.last : nil
     end
 
-    def theorical_intinerary
-      @theorical_intinerary ||= GeoProcessService.new.theorical_itinerary(from, to)
+    def theorical_itinerary
+      @theorical_itinerary ||= GeoProcessService.new.theorical_itinerary(from, to)
     end
 
     def osrm_itinerary
-      # !!! appel API
+      # BE CARFULE : API call
       @osrm_itinerary ||= GeoProcessService.new.osrm_itinerary(from, to)
     end
 
     def itinerary
-      # FIXME: probleme de perf quand trop de points
+      # FIXME: performence issu when to much waypoints
       return @itinerary if @itinerary
       wp = []
       wp << from
@@ -374,30 +377,18 @@ module Web
       wp << to
       @itinerary = Web::Itinerary.new( { waypoints: wp })
       return @itinerary
-
-      #@itinerary ||= theorical_intinerary
     end
 
-    def compatible_dow?(day_of_week)
-      compatible_dow = true
-
-      if day_of_week && day_of_week.any? 
-        unless (day_of_week & (dow ? dow : [])).any? 
-          compatible_dow = false
-        end 
-      end
-
-      return compatible_dow
+    def is_declared_for_dows?(days_ids)
+      days_ids && dow && !(days_ids & dow).empty?
     end
 
     def detour_kilometers
-      # [0, 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 13, 15, 20, 22, 26, 27, 30, 60, 120, 240, 300, 360, 420, 480, 540, 600, 660, 900, 1140]
-      borned_detour_delta = detour_delta
-      # borned_detour_delta = 30 if borned_detour_delta > 30
-      borned_detour_delta = 20 if borned_detour_delta > 20
+      limited_detour_delta = detour_delta
+      limited_detour_delta = DETOUR_DELTA_LIMIT if limited_detour_delta > DETOUR_DELTA_LIMIT
 
-      # detour_delta : approx km detour (A/R point detour) - décorellée des temps de passage
-      detour_commuted = (borned_detour_delta.to_f * (Web::Itinerary::SPEED_AVERAGE.to_f/60.to_f).to_f) / 2 
+      detour_commuted = (limited_detour_delta.to_f * (Web::Itinerary::SPEED_AVERAGE.to_f/60.to_f).to_f) / 2 
+
       detour_min      = Web::Itinerary::KM_DETOUR_MIN.to_f
 
       detour_kilometers = if detour_commuted <= detour_min
@@ -454,37 +445,16 @@ module Web
         @filtered_waypoints = nil
       end
 
-      return [] unless @filtered_waypoints
+      return [] unless @filtered_waypoint
 
-      if @filtered_waypoints.count > 30
-        if @filtered_waypoints.count < 100
-          @filtered_waypoints = @filtered_waypoints.each_slice(10).map(&:last)
-        elsif @filtered_waypoints.count < 100
-          @filtered_waypoints = @filtered_waypoints.each_slice(20).map(&:last)
-        elsif @filtered_waypoints.count < 200
-          @filtered_waypoints = @filtered_waypoints.each_slice(30).map(&:last)
-        elsif @filtered_waypoints.count < 300
-          @filtered_waypoints = @filtered_waypoints.each_slice(40).map(&:last)
-        elsif @filtered_waypoints.count < 400
-          @filtered_waypoints = @filtered_waypoints.each_slice(50).map(&:last)
-        elsif @filtered_waypoints.count < 500
-          @filtered_waypoints = @filtered_waypoints.each_slice(60).map(&:last)
-        elsif @filtered_waypoints.count < 600
-          @filtered_waypoints = @filtered_waypoints.each_slice(70).map(&:last)
-        elsif @filtered_waypoints.count < 700
-          @filtered_waypoints = @filtered_waypoints.each_slice(80).map(&:last)
-        elsif @filtered_waypoints.count < 800
-          @filtered_waypoints = @filtered_waypoints.each_slice(90).map(&:last)
-        elsif @filtered_waypoints.count < 900
-          @filtered_waypoints = @filtered_waypoints.each_slice(100).map(&:last)
-        elsif @filtered_waypoints.count >= 900
-          @filtered_waypoints = @filtered_waypoints.each_slice(110).map(&:last)
-        end
-      else
-        @filtered_waypoints = @filtered_waypoints.each_slice(3).map(&:last)
-      end
+      slice_val = @filtered_waypoints.count > 30 ? ((@filtered_waypoint.count/100)+1)*10 : 3
+      @filtered_waypoints = @filtered_waypoints.each_slice(slice_val).map(&:last)
 
       return @filtered_waypoints
+    end
+
+    def quarters_times_intervals
+      (0..23).each.flat_map { |hour| [["#{hour}:00", "#{hour}:15"], ["#{hour}:15", "#{hour}:30"], ["#{hour}:30", "#{hour}:45"], ["#{hour}:45", "#{ hour+1 < 24 ? hour+1 : 0}:00"]] }
     end
   end
 end
--- app/models/web/commute_whishe.rb
+++ app/models/web/commute_whishe.rb
@@ -1,5 +1,72 @@
 module Web
   class CommuteWhishe < ActiveRecord::Base
+    WHISHE_STATUSES        = { created: 1, confirmed: 3, deleted: -1 }
+    COMMUTE_WHISHE_VERSION = 'v3'
 
+    # attr_accessor :confirmed, :version, :search_dtm, :nb_result, :referer, :remote_ip
+
+    after_initialize :new_commute_whish
+
+    validates :selected_date, presence: true
+    validates :start_id, presence: true
+    validates :end_id, presence: true
+    validates :user_id, presence: true
+    validates :from_name, presence: true
+    validates :to_name, presence: true
+    validates :confirmed, presence: true
+    validates :version, presence: true
+    # validates :referer, presence: true
+    validates :remote_ip, presence: true
+    validates :selected_date, presence: true
+
+    def set_nb_results(nb_results)
+      self.nb_results = nb_results
+    end
+
+    def set_date
+      self.search_dtm = DateTime.now.to_s
+    end
+
+    def deleted?
+      self.confirmed == WHISHE_STATUSES[:deleted]
+    end
+
+    def created?
+      self.confirmed == WHISHE_STATUSES[:created]
+    end
+
+    def set_confirmed
+      set_status(:confirmed)
+    end
+
+    def set_deleted
+      set_status(:deleted)
+    end
+
+    def set_created
+      set_status(:created)
+    end
+
+    private
+
+    def new_commute_whish
+      return unless self.new_record?
+
+      set_created
+      initialize_version
+      set_date
+    end
+
+    def set_status(status)
+      self.confirmed = WHISHE_STATUSES[status]
+    end
+
+    def initialize_status
+      set_created unless self.confirmed
+    end
+
+    def initialize_version
+      self.version = COMMUTE_WHISHE_VERSION unless self.version
+    end
   end
 end
