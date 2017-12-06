797c9af194b20aa85fef7f41aaf4248422237aa6
Karim Naghmouchi <karim@ecov.fr>
2017-12-06 11:53:38 +0100

    ok

--- app/models/concerns/has_si_synchronization.rb
+++ app/models/concerns/has_si_synchronization.rb
@@ -90,7 +90,7 @@ module Concerns
         if valid?
           si_response = ManageSIModelService.new(si_class_name, @token, processed_attributes).put
 
-          return si_response.inspect
+          return si_response.results.inspect
 
           if si_response && si_response.response_object && si_response.response_object['results'] && si_response.response_object['results']['id']
             return si_response.response_object['results']['id']
